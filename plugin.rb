5fc45910b73577f8637e6cf739baa7062b35f348
Benjamin Kampmann <ben.kampmann@gmail.com>
2014-12-28 17:59:47 +0100

    Init with super simple Sidebar Theme

---
+++ plugin.rb
@@ -0,0 +1,7 @@
+# name: simple sidebar plugin
+# about: shows a curated home in a different style
+# version: 0.1
+# authors: Benjamin Kampmann
+
+
+register_asset "javascripts/discourse/templates/application.hbs"
